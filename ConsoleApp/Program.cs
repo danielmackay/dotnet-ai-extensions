eed92d085128502c7fb57cf648bb8f06f06fbb4e
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2024-12-28 08:06:26 +1000

    🎉 Initial commit

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using Azure;
+using Azure.AI.Inference;
+using Azure.AI.OpenAI;
+using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Configuration;
+
+var config = new ConfigurationBuilder()
+    .AddUserSecrets<Program>()
+    .Build();
+
+var endpoint = new Uri("https://models.inference.ai.azure.com");
+var credential = new AzureKeyCredential(config["GH_PAT"]!);
+
+IChatClient client =
+    new ChatCompletionsClient(
+            endpoint: endpoint,
+            credential)
+        .AsChatClient("Phi-3.5-MoE-instruct");
+
+var response = await client.CompleteAsync("What is AI?");
+
+Console.WriteLine(response.Message);
+
+IEmbeddingGenerator<string,Embedding<float>> generator =
+    new AzureOpenAIClient(
+            endpoint,
+            credential)
+        .AsEmbeddingGenerator(modelId: "text-embedding-3-small");
+
+var embeddings = await generator.GenerateAsync(["What is AI?"]);
+
+Console.WriteLine(string.Join(", ", embeddings[0].Vector.ToArray()));
---
+++ readme.md
@@ -0,0 +1,7 @@
+# .NET Microsoft.AI.Extensions Sample
+
+Sample project to show off chat completion and embeddings generation using the Microsoft.AI.Extensions library and GitHub models.
+
+## Resources
+
+https://devblogs.microsoft.com/dotnet/introducing-microsoft-extensions-ai-preview/#embeddings
